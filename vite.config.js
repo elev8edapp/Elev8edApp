8ddccf6859cd689c6bfa3e6df288ed7548efe427
Talin <elev8ed.app1@gmail.com>
2025-08-10 17:20:52 +1000

    Try basic update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Elev8edApp/',
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
 })
