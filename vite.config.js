c12a453ba042db48c40d60a1ad26c1ec38401dbc
Talin <elev8ed.app1@gmail.com>
2025-08-10 17:17:57 +1000

    Update vite to use import instead of require

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Elev8edApp/', // Replace with your actual repository name
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
